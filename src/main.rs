38dad6f4951794621273cafb0cc5ba0161c31db4
Brendan Ware <bware@mobileiron.com>
2020-07-14 15:25:54 -0400

    Step 1 - Allow passing file path as param, and validate the file exists

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{env, process, path};
+use std::ffi::OsString;
+
+const UTIL_NAME: &str = "Rust Archive Util";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<OsString> = env::args_os().collect();
+
+    if args.len() < 2 {
+        eprintln!("{} - Missing Required argument <File path>", UTIL_NAME);
+        process::exit(1);
+    }
+
+    let filepath = path::Path::new(&args[1]);
+
+    if !filepath.exists() {
+        eprintln!("{} - Parameter {:?}: File does not exist", UTIL_NAME, filepath);
+        process::exit(1);
+    }
+
+    println!("{:?}", filepath);
 }
