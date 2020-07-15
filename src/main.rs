ff5ba680857d99c3b1045532a24a3dbfc12fc6eb
Brendan Ware <bware@mobileiron.com>
2020-07-15 10:59:01 -0400

    Step 1 - Add basic unit testing

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,65 @@
-use std::{env, process, path};
 use std::ffi::OsString;
+use std::path::{Path, PathBuf};
+use std::{env, process};
 
 const UTIL_NAME: &str = "Rust Archive Util";
 
 fn main() {
-    let args: Vec<OsString> = env::args_os().collect();
+    let filepath = match fetch_path(env::args_os().collect()) {
+        None => {
+            eprintln!("{} - Missing Required argument <File path>", UTIL_NAME);
+            process::exit(1);
+        }
+        Some(path) => path,
+    };
 
-    if args.len() < 2 {
-        eprintln!("{} - Missing Required argument <File path>", UTIL_NAME);
-        process::exit(1);
-    }
-
-    let filepath = path::Path::new(&args[1]);
-
-    if !filepath.exists() {
-        eprintln!("{} - Parameter {:?}: File does not exist", UTIL_NAME, filepath);
+    if valid_path(filepath.as_path()) {
+        eprintln!(
+            "{} - Parameter {:?}: File does not exist",
+            UTIL_NAME, filepath
+        );
         process::exit(1);
     }
 
     println!("{:?}", filepath);
+}
+
+fn fetch_path(args: Vec<OsString>) -> Option<PathBuf> {
+    if args.len() < 2 {
+        None
+    } else {
+        Some(PathBuf::from(&args[1]))
+    }
+}
+
+fn valid_path(filepath: &Path) -> bool {
+    filepath.exists()
+}
+
+#[test]
+fn fetch_path_test() {
+    let expected_none = fetch_path(Vec::new());
+    assert_eq!(expected_none, None);
+
+    let args: Vec<OsString> = vec![OsString::from("test"), OsString::from("test2")];
+
+    match fetch_path(args) {
+        None => assert!(false),
+        Some(p) => assert_eq!(p.as_path().as_os_str(), OsString::from("test2")),
+    }
+}
+
+#[test]
+fn test_valid_path() {
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("tests/resources");
+
+    let mut valid = PathBuf::from(d.clone());
+    valid.push("test.txt");
+
+    let mut invalid = PathBuf::from(d);
+    invalid.push("invalid.txt");
+
+    assert_eq!(valid_path(valid.as_path()), true);
+    assert_eq!(valid_path(invalid.as_path()), false);
 }
